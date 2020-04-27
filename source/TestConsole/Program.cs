28ed5b912e77231aed33d73c75053564f82be811
Mark Pflug <MarkPflug@users.noreply.github.com>
2020-04-27 10:13:53 -0700

    Sysinfo tool (#4)
    * merge sysinfo tool into repo
    
    * colorconsole
    
    * clean up sysinfo tool

---
+++ source/TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sylvan.Terminal;
+
+namespace TestConsole
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+
+			var c = new ColorConsole();
+			c.SetForeground(0xa0, 0xa0, 0x20);
+			c.Write("Hello, world");
+        }
+    }
+}
