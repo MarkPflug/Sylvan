2f5ef36f8eb903bdabeb15bc5b601448fc41c32c
MarkPflug <mark.pflug@live.com>
2020-04-29 16:20:05 -0700

    CsvWriter fixes

--- source/Sylvan.Data.Csv/CsvDataWriter.cs
+++ source/Sylvan.Data.Csv/CsvDataWriter.cs
@@ -8,8 +8,8 @@ namespace Sylvan.Data.Csv
 {
 	public sealed class CsvDataWriter
 	{
-		readonly CsvWriter writer;		
-		
+		readonly CsvWriter writer;
+
 		public CsvDataWriter(TextWriter writer, CsvWriterOptions? options = null)
 		{
 			if (writer == null) throw new ArgumentNullException(nameof(writer));
@@ -49,74 +49,85 @@ namespace Sylvan.Data.Csv
 						type = typeCode
 					};
 			}
-			
+
 			for (int i = 0; i < c; i++)
 			{
 				var header = reader.GetName(i);
 				await writer.WriteFieldAsync(header);
 			}
 			await writer.EndRecordAsync();
-
+			int row = 0;
 			cancel.ThrowIfCancellationRequested();
 			while (await reader.ReadAsync(cancel))
 			{
-				for (int i = 0; i < c; i++)
+				row++;
+				int i = 0; // field
+				try
 				{
-					var typeCode = fieldTypes[i].type;
-					var allowNull = fieldTypes[i].allowNull;
+					for (; i < c; i++)
+					{
+						var typeCode = fieldTypes[i].type;
+						var allowNull = fieldTypes[i].allowNull;
 
-					if (allowNull && reader.IsDBNull(i)) // TODO: async?
-					{
-						await writer.WriteFieldAsync("");
-						break;
-					}
-					long intVal;
-					string? str;
-						
-					switch (typeCode)
-					{
-						case TypeCode.Boolean:
-							str = reader.GetBoolean(i) ? "true" : "false";
-							goto str;
-						case TypeCode.String:
-							str = reader.GetString(i);
-							goto str;
-						case TypeCode.Byte:
-							intVal = reader.GetByte(i);
-							goto intVal;
-						case TypeCode.Int16:
-							intVal = reader.GetInt16(i);
-							goto intVal;
-						case TypeCode.Int32:
-							intVal = reader.GetInt32(i);
-							goto intVal;
-						case TypeCode.Int64:
-							intVal = reader.GetInt64(i);
-						intVal:
-							await writer.WriteFieldAsync(intVal);
-							break;
-						case TypeCode.DateTime:
-							var dateVal = reader.GetDateTime(i);
-							await writer.WriteFieldAsync(dateVal);
-							break;
-						case TypeCode.Single:
-							var floatVal = reader.GetFloat(i);
-							await writer.WriteFieldAsync(floatVal);
-							break;
-						case TypeCode.Double:
-							var doubleVal = reader.GetDouble(i);
-							await writer.WriteFieldAsync(doubleVal);
-							break;
-						case TypeCode.Empty:
-						case TypeCode.DBNull:
+						if (allowNull && reader.IsDBNull(i)) // TODO: async?
+						{
 							await writer.WriteFieldAsync("");
-							break;
-						default:
-							str = reader.GetValue(i)?.ToString() ?? "";
-						str:
-							await writer.WriteFieldAsync(str);
-							break;
+							continue;
+						}
+						int intVal;
+						string? str;
+
+						switch (typeCode)
+						{
+							case TypeCode.Boolean:
+								var boolVal = reader.GetBoolean(i);
+								await writer.WriteFieldAsync(boolVal);
+								break;
+							case TypeCode.String:
+								str = reader.GetString(i);
+								goto str;
+							case TypeCode.Byte:
+								intVal = reader.GetByte(i);
+								goto intVal;
+							case TypeCode.Int16:
+								intVal = reader.GetInt16(i);
+								goto intVal;
+							case TypeCode.Int32:
+								intVal = reader.GetInt32(i);
+							intVal:
+								await writer.WriteFieldAsync(intVal);
+								break;
+							case TypeCode.Int64:
+								var longVal = reader.GetInt64(i);
+								await writer.WriteFieldAsync(longVal);
+								break;
+							case TypeCode.DateTime:
+								var dateVal = reader.GetDateTime(i);
+								await writer.WriteFieldAsync(dateVal);
+								break;
+							case TypeCode.Single:
+								var floatVal = reader.GetFloat(i);
+								await writer.WriteFieldAsync(floatVal);
+								break;
+							case TypeCode.Double:
+								var doubleVal = reader.GetDouble(i);
+								await writer.WriteFieldAsync(doubleVal);
+								break;
+							case TypeCode.Empty:
+							case TypeCode.DBNull:
+								await writer.WriteFieldAsync("");
+								break;
+							default:
+								str = reader.GetValue(i)?.ToString() ?? "";
+							str:
+								await writer.WriteFieldAsync(str);
+								break;
+						}
 					}
+				}
+				catch (ArgumentOutOfRangeException e)
+				{
+					throw new CsvRecordTooLargeException(row, i, null, e);
 				}
 
 				await writer.EndRecordAsync();
@@ -124,9 +135,8 @@ namespace Sylvan.Data.Csv
 				cancel.ThrowIfCancellationRequested();
 			}
 			// flush any pending data on the way out.
-			await writer.FlushAsync();			
+			await writer.FlushAsync();
 		}
-
 
 		public void Write(DbDataReader reader)
 		{
@@ -153,66 +163,78 @@ namespace Sylvan.Data.Csv
 				writer.WriteField(header);
 			}
 			writer.EndRecord();
-
+			int row = 0;
 			while (reader.Read())
 			{
-				for (int i = 0; i < c; i++)
+				row++;
+				int i = 0; // field
+				try
 				{
-					var typeCode = fieldTypes[i].type;
-					var allowNull = fieldTypes[i].allowNull;
-
-					if (allowNull && reader.IsDBNull(i)) // TODO: async?
+					for (; i < c; i++)
 					{
-						writer.WriteField("");
-						break;
-					}
-					long intVal;
-					string? str;
+						var allowNull = fieldTypes[i].allowNull;
 
-					switch (typeCode)
-					{
-						case TypeCode.Boolean:
-							str = reader.GetBoolean(i) ? "true" : "false";
-							goto str;
-						case TypeCode.String:
-							str = reader.GetString(i);
-							goto str;
-						case TypeCode.Byte:
-							intVal = reader.GetByte(i);
-							goto intVal;
-						case TypeCode.Int16:
-							intVal = reader.GetInt16(i);
-							goto intVal;
-						case TypeCode.Int32:
-							intVal = reader.GetInt32(i);
-							goto intVal;
-						case TypeCode.Int64:
-							intVal = reader.GetInt64(i);
-						intVal:
-							writer.WriteField(intVal);
-							break;
-						case TypeCode.DateTime:
-							var dateVal = reader.GetDateTime(i);
-							writer.WriteField(dateVal);
-							break;
-						case TypeCode.Single:
-							var floatVal = reader.GetFloat(i);
-							writer.WriteField(floatVal);
-							break;
-						case TypeCode.Double:
-							var doubleVal = reader.GetDouble(i);
-							writer.WriteField(doubleVal);
-							break;
-						case TypeCode.Empty:
-						case TypeCode.DBNull:
+						if (allowNull && reader.IsDBNull(i)) // TODO: async?
+						{
 							writer.WriteField("");
-							break;
-						default:
-							str = reader.GetValue(i)?.ToString() ?? "";
-						str:
-							writer.WriteField(str);
-							break;
+							continue;
+						}
+
+						var typeCode = fieldTypes[i].type;
+						int intVal;
+						string? str;
+
+						switch (typeCode)
+						{
+							case TypeCode.Boolean:
+								var boolVal = reader.GetBoolean(i);
+								writer.WriteField(boolVal);
+								break;
+							case TypeCode.String:
+								str = reader.GetString(i);
+								goto str;
+							case TypeCode.Byte:
+								intVal = reader.GetByte(i);
+								goto intVal;
+							case TypeCode.Int16:
+								intVal = reader.GetInt16(i);
+								goto intVal;
+							case TypeCode.Int32:
+								intVal = reader.GetInt32(i);
+							intVal:
+								writer.WriteField(intVal);
+								break;
+							case TypeCode.Int64:
+								var longVal = reader.GetInt64(i);
+								writer.WriteField(longVal);
+								break;
+							case TypeCode.DateTime:
+								var dateVal = reader.GetDateTime(i);
+								writer.WriteField(dateVal);
+								break;
+							case TypeCode.Single:
+								var floatVal = reader.GetFloat(i);
+								writer.WriteField(floatVal);
+								break;
+							case TypeCode.Double:
+								var doubleVal = reader.GetDouble(i);
+								writer.WriteField(doubleVal);
+								break;
+							case TypeCode.Empty:
+							case TypeCode.DBNull:
+								writer.WriteField("");
+								break;
+							default:
+								str = reader.GetValue(i)?.ToString() ?? "";
+							str:
+								writer.WriteField(str);
+								break;
+						}
 					}
+				}
+				catch (ArgumentOutOfRangeException e)
+				{
+					throw new CsvRecordTooLargeException(row, i, null, e);
 				}
 
 				writer.EndRecord();
