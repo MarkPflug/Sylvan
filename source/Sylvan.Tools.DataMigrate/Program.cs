680b4bc0c2e3b7aeb7830c3106e2e34a91efb1a8
MarkPflug <mark.pflug@live.com>
2020-05-02 02:26:00 -0700

    mysql migration experiment

--- source/Sylvan.Tools.DataMigrate/Program.cs
+++ source/Sylvan.Tools.DataMigrate/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using Sylvan.Data;
 using Sylvan.Data.Csv;
 using Sylvan.Terminal;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
@@ -37,10 +39,185 @@ namespace Sylvan.Tools.DataMigrate
 
 		static void Main(string[] args)
 		{
+			SqlToCsv();
+		}
+
+		static MySqlConnection GetMySqlConnection()
+		{
+			var connStr = Environment.GetEnvironmentVariable("MySqlConnStr");
+			var csb = new MySqlConnectionStringBuilder(connStr);
+			csb.Database = "floop";
+			csb.AllowLoadLocalInfile = true;
+			connStr = csb.ToString();
+			var conn = new MySqlConnection(connStr);
+			conn.Open();
+			return conn;
+		}
+
+		static void SqlToMySql()
+		{
 			var csb = new SqlConnectionStringBuilder
 			{
 				DataSource = ".",
-				InitialCatalog = "ccexport",
+				InitialCatalog = "scdev",
+				IntegratedSecurity = true,
+				MultipleActiveResultSets = true,
+			};
+
+			using var conn = new SqlConnection(csb.ConnectionString);
+			conn.Open();
+			var cmd = conn.CreateCommand();
+
+			cmd.CommandText = "select table_schema, table_name from information_schema.tables where table_type = 'BASE TABLE'";
+			using var reader = cmd.ExecuteReader();
+
+			var dc = conn.CreateCommand();
+
+			var mconn = GetMySqlConnection();
+			var mcmd = mconn.CreateCommand();
+
+			var t1 = Stopwatch.StartNew();
+			while (reader.Read())
+			{
+				var schema = reader.GetString(0);
+				var tableName = reader.GetString(1);
+
+				dc.CommandText = "select * from [" + schema + "].[" + tableName + "]";
+				using var dr = dc.ExecuteReader();
+
+				var sw = new StringWriter();
+				CreateTable(sw, tableName, dr);
+
+				var str = sw.ToString();
+				mcmd.CommandText = str;
+				mcmd.ExecuteNonQuery();
+
+				Console.Write("writing " + tableName);
+
+				var t2 = Stopwatch.StartNew();
+				var bc = new MySqlBulkCopy(mconn);
+				bc.BulkCopyTimeout = 0;
+				bc.DestinationTableName = tableName;
+				bc.WriteToServer(dr);
+				t2.Stop();
+				Console.WriteLine(t2.Elapsed.ToString());
+			}
+			t1.Stop();
+			Console.WriteLine(t1.Elapsed.ToString());
+		}
+
+		static void CreateTable(TextWriter writer, string table, IDbColumnSchemaGenerator schema)
+		{
+			var cols = schema.GetColumnSchema();
+
+			writer.WriteLine("create table " + table + "(");
+
+			bool first = true;
+			foreach (var col in cols)
+			{
+				if (first == true)
+				{
+					first = false;
+				}
+				else
+				{
+					writer.WriteLine(',');
+				}
+
+				writer.Write("  ");
+				writer.Write(col.ColumnName);
+				writer.Write(" ");
+				switch (Type.GetTypeCode(col.DataType))
+				{
+					case TypeCode.Boolean:
+						writer.Write("bit");
+						break;
+					case TypeCode.DateTime:
+						writer.Write("datetime");
+						break;
+					case TypeCode.Byte:
+						writer.Write("tinyint");
+						break;
+					case TypeCode.Int16:
+						writer.Write("smallint");
+						break;
+					case TypeCode.Int32:
+						writer.Write("int");
+						break;
+					case TypeCode.Int64:
+						writer.Write("bigint");
+						break;
+					case TypeCode.String:
+						if (col.IsLong == true)
+						{
+							writer.Write("text");
+						}
+						else
+						{
+							writer.Write("varchar");
+							writer.Write('(');
+							writer.Write(Math.Min(255, col.ColumnSize ?? 255));
+							writer.Write(')');
+						}
+						break;
+					case TypeCode.Object:
+						if (col.DataType == typeof(byte[]))
+						{
+							if (col.IsLong == true)
+							{
+								writer.Write("blob");
+							}
+							else
+							{
+								if (IsFixed(col))
+								{
+									writer.Write("binary");
+								}
+								else
+								{
+									writer.Write("varbinary");
+								}
+								writer.Write('(');
+								writer.Write(col.ColumnSize ?? 255);
+								writer.Write(')');
+							}
+						}
+						else
+						{
+
+						}
+						break;
+					default:
+
+						break;
+				}
+				writer.Write(' ');
+				if (col.AllowDBNull == false)
+				{
+					writer.Write("not null");
+				}
+				else
+				{
+					writer.Write("null");
+				}
+
+			}
+			writer.WriteLine();
+			writer.WriteLine(")");
+			writer.WriteLine();
+		}
+
+		static bool IsFixed(DbColumn col)
+		{
+			return col.DataTypeName == "binary";
+		}
+
+		static void SqlToCsv()
+		{
+			var csb = new SqlConnectionStringBuilder
+			{
+				DataSource = ".",
+				InitialCatalog = "scdev",
 				IntegratedSecurity = true,
 				MultipleActiveResultSets = true,
 			};
@@ -108,23 +285,25 @@ namespace Sylvan.Tools.DataMigrate
 					using var dr = dc.ExecuteReader();
 
 					using var ts = new StreamWriter(os, Encoding.UTF8);
-					Action<long> cb = (r) => { 
-						progress += rowInc; 
-						pp += rowInc; 
-						if (pp > updateT) { 
+					Action<long> cb = (r) =>
+					{
+						progress += rowInc;
+						pp += rowInc;
+						if (pp > updateT)
+						{
 							pp -= updateT;
-							
+
 							var p = progress * 100.0 / totalSize;
-							
+
 							var e = sw.Elapsed;
 							var total = e.Ticks * totalSize / progress;
 							var time = TimeSpan.FromTicks(total);
-					
+
 							cc.SetCursorPosition(0, 2);
 							cc.WriteLine($"                                                                                                          ");
 							cc.SetCursorPosition(0, 2);
 							cc.Write($"{p,5:0.0}% {(int)e.TotalSeconds}/{(int)time.TotalSeconds}");
-						} 
+						}
 					};
 					var csvw = new CsvDataWriter(ts, opts, cb);
 					csvw.Write(dr);
