575c93b861052068a4064f7b7036b3254927d9e1
MarkPflug <mark.pflug@live.com>
2020-05-21 12:37:16 -0700

    refactor classes into separate files

---
+++ source/Sylvan.Common/IO/Encoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sylvan.IO
+{
+	enum EncoderResult
+	{
+		Flush,
+		RequiresInput,
+		RequiresOutputSpace,
+		Complete,
+	}
+
+	abstract class Encoder
+	{
+		public abstract EncoderResult Encode(ReadOnlySpan<byte> src, Span<byte> dst, out int bytesConsumed, out int bytesWritten);
+	}
+}
