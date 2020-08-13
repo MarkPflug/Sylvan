a8aa46596eb7139ef20018d7a0e190c6c30febac
MarkPflug <mark.pflug@live.com>
2020-08-13 08:32:06 -0700

    crappy binder impl

---
+++ source/Sylvan.Data.Tests/DataBinderTests.cs
@@ -0,0 +1,56 @@
+﻿using Sylvan.Data.Csv;
+using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using Xunit;
+
+namespace Sylvan.Data
+{
+
+	class MyDataRecord
+	{
+		[ColumnOrdinal(0)]
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public DateTime? Date { get; private set; }
+	}
+
+	public class DataBinderTests
+	{
+		static readonly Lazy<ReadOnlyCollection<DbColumn>> Schema = new Lazy<ReadOnlyCollection<DbColumn>>(() => BuildSchema());
+
+		static ReadOnlyCollection<DbColumn> BuildSchema()
+		{
+			var sb = new Schema.Builder();
+			sb.AddColumn("Id", DbType.Int32, false);
+			sb.AddColumn("Name", DbType.String, false);
+			sb.AddColumn("Date", DbType.DateTime, true);
+			var schema = sb.Build();
+			return schema.GetColumnSchema();
+		}
+
+		static void Bind()
+		{
+
+		}
+
+		[Fact]
+		public void Test1()
+		{
+			var schema = BuildSchema();
+			var binder = new DataBinder<MyDataRecord>(schema);
+
+			var csvData = "Id,Name,Date\n1,Test,2020-08-12\n";
+			var tr = new StringReader(csvData);
+			var opts = new CsvDataReaderOptions() { Schema = new CsvSchema(schema) };
+			DbDataReader data = CsvDataReader.Create(tr, opts);
+
+			while (data.Read())
+			{
+				var item = binder.Bind(data);
+			}
+		}
+	}
+}
---
+++ source/Sylvan.Data/DataBinder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+
+namespace Sylvan.Data
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+	public sealed class ColumnOrdinalAttribute : Attribute
+	{
+		public int Ordinal { get; }
+		public ColumnOrdinalAttribute(int ordinal)
+		{
+			this.Ordinal = ordinal;
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+	public sealed class ColumnNameAttribute : Attribute
+	{
+		public string Name { get; }
+		public ColumnNameAttribute(string name)
+		{
+			this.Name = name;
+		}
+	}
+
+	public sealed class DataBinder<T> where T : class, new()
+	{
+		readonly ReadOnlyCollection<DbColumn> schema;
+		readonly DbColumn[] columns;
+
+		public DataBinder(ReadOnlyCollection<DbColumn> schema)
+		{
+			this.schema = schema;
+
+			// or just schema.ToArray()?
+			this.columns =
+				Enumerable
+				.Range(0, schema.Count)
+				.Select(i => schema.FirstOrDefault(c => c.ColumnOrdinal == i))
+				.ToArray();
+			
+		}
+
+		public T Bind(IDataRecord record)
+		{
+			var item = new T();
+
+			var type = typeof(T);
+
+			var args = new object?[1];
+			foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+			{
+				args[0] = null;
+				var columnOrdinal = property.GetCustomAttribute<ColumnOrdinalAttribute>()?.Ordinal;
+				var columnName = property.GetCustomAttribute<ColumnNameAttribute>()?.Name ?? property.Name;
+
+				var setter = property.GetSetMethod(true);
+
+				var paramType = setter.GetParameters()[0].ParameterType;
+				var ordinal =
+					columnName != null
+					? record.GetOrdinal(columnName)
+					: columnOrdinal ?? -1;
+				if (ordinal == -1) throw new Exception("Binding failure");
+
+				var col = columns[ordinal];
+
+				var isNull = col.AllowDBNull != false && record.IsDBNull(ordinal);
+
+				if (!isNull)
+				{
+					args[0] = record.GetValue(ordinal);
+					setter.Invoke(item,  args);
+				}
+			}
+
+			return item;
+		}
+	}
+}
