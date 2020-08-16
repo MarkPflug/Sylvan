2dba3419ff57c824fdb4d0de8770ba53e2fd7fdd
MarkPflug <mark.pflug@live.com>
2020-08-16 14:31:41 -0700

    string pool test

--- source/Sylvan.Common.Tests/StringPoolTest.cs
+++ source/Sylvan.Common.Tests/StringPoolTest.cs
@@ -1,32 +1,19 @@
-﻿//using Xunit;
+﻿using Xunit;
 
-//namespace Sylvan
-//{
-//	public class StringPoolTest
-//	{
-
-//		[Fact]
-//		public void Test1()
-//		{
-//			var sp = new StringPool(32);
-//			var data = new char[] { 'a', 'b', 'c', 'd' };
-//			var str = sp.GetString(data);
-//			Assert.NotNull(str);
-//			Assert.Equal("abcd", str);
-//			var str2 = sp.GetString(data);
-//			Assert.Same(str, str2);
-//		}
-
-//		[Fact]
-//		public void Test2()
-//		{
-//			var sp = new StringPoolFast(32);
-//			var data = new char[] { 'a', 'b', 'c', 'd' };
-//			var str = sp.GetString(data);
-//			Assert.NotNull(str);
-//			Assert.Equal("abcd", str);
-//			var str2 = sp.GetString(data);
-//			Assert.Same(str, str2);
-//		}
-//	}
-//}
+namespace Sylvan
+{
+	public class StringPoolTest
+	{
+		[Fact]
+		public void DeDupeTest()
+		{
+			var sp = new StringPool(32);
+			var data = new char[] { 'a', 'b', 'c', 'd' };
+			var str = sp.GetString(data, 0, 4);
+			Assert.NotNull(str);
+			Assert.Equal("abcd", str);
+			var str2 = sp.GetString(data, 0, 4);
+			Assert.Same(str, str2);
+		}
+	}
+}
