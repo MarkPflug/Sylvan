83128ab5d313bfae8a5cddfcee1b05f4d967c560
Mark Pflug <MarkPflug@users.noreply.github.com>
2021-01-09 08:33:09 -0800

    numeric parsing perf (#35)
    * optimize numeric values. fix parsing out of range values for rows missing columns.
    
    * datetime values with offset will be returned in UTC
    
    * refactor autodetect option

--- source/Sylvan.Data.Csv.Tests/CsvDataReaderTests.cs
+++ source/Sylvan.Data.Csv.Tests/CsvDataReaderTests.cs
@@ -394,15 +394,14 @@ namespace Sylvan.Data.Csv
 		{
 			public DbColumn GetColumn(string name, int ordinal)
 			{
-				return new ExcelColumn("" + (char)('A' + ordinal), ordinal);
+				return new ExcelColumn("" + (char)('A' + ordinal));
 			}
 
 			class ExcelColumn : DbColumn
 			{
-				public ExcelColumn(string name, int ordinal)
+				public ExcelColumn(string name)
 				{
 					this.ColumnName = name;
-					this.ColumnOrdinal = ordinal;
 				}
 			}
 		}
@@ -452,7 +451,6 @@ namespace Sylvan.Data.Csv
 					this.ColumnName = name;
 				}
 			}
-
 
 			public DbColumn GetColumn(string name, int ordinal)
 			{
@@ -902,17 +900,36 @@ namespace Sylvan.Data.Csv
 		[Fact]
 		public void CultureWithCommaDecimal()
 		{
-			using var reader = new StringReader("Name;Value1;Value2\nTest;2,08;0,82\n");
+			using var reader = new StringReader("Name;Value1;Value2\nTest;2,08;0,82\nB;1;2");
 
 			var schema = Schema.Parse("Name,Value1:float,Value2:float");
 
-			var options = new CsvDataReaderOptions {
+			var options = new CsvDataReaderOptions
+			{
 				Schema = new CsvSchema(schema),
 				Culture = CultureInfo.GetCultureInfoByIetfLanguageTag("it-IT")
 			};
 			var csv = CsvDataReader.Create(reader, options);
-			var dt = new DataTable();
-			dt.Load(csv);
+			Assert.True(csv.Read());
+			Assert.Equal(2.08f, csv.GetFloat(1));
+			Assert.Equal(0.82f, csv.GetFloat(2));
+			Assert.True(csv.Read());
+			Assert.Equal(1, csv.GetFloat(1));
+			Assert.Equal(2, csv.GetFloat(2));
+		}
+		
+		[Fact]
+		public void Binary2()
+		{
+			using var reader = new StringReader("Name,Value\r\nrow1,abcdefgh");
+			var csv = CsvDataReader.Create(reader);
+			csv.Read();
+			var buf = new byte[0x100];
+			var len = csv.GetBytes(1, 0, null, 0, 0);
+			Assert.Equal(6, len);
+			len = csv.GetBytes(1, 0, buf, 0, buf.Length);
+			Assert.Equal(6, len);
+
 		}
 	}
 }
