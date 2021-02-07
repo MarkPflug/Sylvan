fcb8c1b7477bc9649a2a4265810a33cc5621a176
MarkPflug <mark.pflug@live.com>
2021-02-06 22:49:56 -0800

    remove obsolete tests.

--- source/Sylvan.Data.Csv.Tests/CsvWriterTests.cs
+++ source/Sylvan.Data.Csv.Tests/CsvWriterTests.cs
@@ -15,67 +15,6 @@ namespace Sylvan.Data.Csv
 			return File.ReadAllText(@"Data\WriterExpected.csv");
 		}
 
-		// these tests are a problem in net 461 because of changes to float formatting
-
-#if NETCOREAPP3_1
-		[Fact]
-		public async Task Async()
-		{
-			var tw = new StringWriter();
-			var items = TestData.GetTestObjects(3, 1);
-			using (var csv = new CsvWriter(tw))
-			{
-				await csv.WriteFieldAsync("Id");
-				await csv.WriteFieldAsync("Name");
-				await csv.WriteFieldAsync("Date");
-				await csv.WriteFieldAsync("IsActive");
-				await csv.WriteFieldAsync("Value");
-				await csv.EndRecordAsync();
-				foreach (var item in items)
-				{
-					await csv.WriteFieldAsync(item.Id);
-					await csv.WriteFieldAsync(item.Name);
-					await csv.WriteFieldAsync(item.Date);
-					await csv.WriteFieldAsync(item.IsActive);
-					await csv.WriteFieldAsync(item.DataSet[0]);
-					await csv.EndRecordAsync();
-				}
-			}
-
-			var expected = GetExpected();
-			Assert.Equal(expected, tw.ToString());
-		}
-
-		[Fact]
-		public void Sync()
-		{
-			var tw = new StringWriter();
-			var items = TestData.GetTestObjects(3, 1);
-			using (var csv = new CsvWriter(tw))
-			{
-				csv.WriteField("Id");
-				csv.WriteField("Name");
-				csv.WriteField("Date");
-				csv.WriteField("IsActive");
-				csv.WriteField("Value");
-				csv.EndRecord();
-				foreach (var item in items)
-				{
-					csv.WriteField(item.Id);
-					csv.WriteField(item.Name);
-					csv.WriteField(item.Date);
-					csv.WriteField(item.IsActive);
-					csv.WriteField(item.DataSet[0]);
-					csv.EndRecord();
-				}
-
-			}
-			var expected = GetExpected();
-			Assert.Equal(expected, tw.ToString());
-		}
-
-#endif
-
 		//[Fact]
 		//public void WriteQuote()
 		//{
