1ceae376e50c26fb0b1f11ed18eb87ed34b2f714
Mark Pflug <MarkPflug@users.noreply.github.com>
2022-01-05 07:45:29 -0800

    ExtendedDataReader implementation (#100)

---
+++ source/Sylvan.Data.Tests/ExtendedDataReaderTests.cs
@@ -0,0 +1,29 @@
+﻿using Sylvan.Data.Csv;
+using System;
+using System.Data.Common;
+using System.IO;
+using Xunit;
+namespace Sylvan.Data
+{
+	public class ExtendedDataReaderTests
+	{
+		[Fact]
+		public void Test1()
+		{
+			var schema = Schema.Parse("Name:string,Number:int,Date:date");
+			var opts = new CsvDataReaderOptions { Schema = new CsvSchema(schema) };
+			var csv = new StringReader("a,b,c\na,1010,2022-01-01\nb,1020,2022-01-02");
+			CsvDataReader csvReader = CsvDataReader.Create(csv, opts);
+			DateTime importDate = new DateTime(2022, 1, 3);
+			var data = csvReader.WithColumns(
+				new CustomDataColumn<DateTime>("ImportDate", r => importDate),
+				new CustomDataColumn<int>("RowNum", r => csvReader.RowNumber)
+			);
+			var sw = new StringWriter();
+			var wo = new CsvDataWriterOptions { NewLine = "\n" };
+			var csvWriter = CsvDataWriter.Create(sw, wo);
+			csvWriter.Write(data);
+			Assert.Equal("Name,Number,Date,ImportDate,RowNum\na,1010,2022-01-01,2022-01-03,1\nb,1020,2022-01-02,2022-01-03,2\n", sw.ToString());			
+		}
+	}
+}
--- source/Sylvan.Data/DbDataReaderTextReader.cs
+++ source/Sylvan.Data/DbDataReaderTextReader.cs
@@ -1,46 +1,43 @@
 ﻿using System;
+using System.Data.Common;
 using System.IO;
 
 namespace Sylvan.Data
 {
-	partial class DataReaderAdapter
+	sealed class DbDataReaderTextReader : TextReader
 	{
-		// supports DbDataReaderAdapter.GetTextReader
-		sealed class DbDataReaderTextReader : TextReader
+		readonly DbDataReader reader;
+		readonly int ordinal;
+		readonly char[] peekBuffer;
+		long position;
+
+		public DbDataReaderTextReader(DbDataReader reader, int ordinal)
 		{
-			readonly DataReaderAdapter reader;
-			readonly int ordinal;
-			readonly char[] peekBuffer;
-			long position;
+			this.reader = reader;
+			this.ordinal = ordinal;
+			this.position = 0;
+			this.peekBuffer = new char[1];
+		}
 
-			public DbDataReaderTextReader(DataReaderAdapter reader, int ordinal)
-			{
-				this.reader = reader;
-				this.ordinal = ordinal;
-				this.position = 0;
-				this.peekBuffer = new char[1];
-			}
+		public override int Read(char[] buffer, int index, int count)
+		{
+			var c = reader.GetChars(ordinal, position, buffer, index, count);
+			position += c;
+			return (int)c;
+		}
 
-			public override int Read(char[] buffer, int index, int count)
-			{
-				var c = reader.GetChars(ordinal, position, buffer, index, count);
-				position += c;
-				return (int)c;
-			}
+		public override int Peek()
+		{
+			throw new NotSupportedException();
+			//var l = Read(peekBuffer!, 0, 1);
+			//position -= l;
+			//return l == 0 ? -1 : peekBuffer![0];
+		}
 
-			public override int Peek()
-			{
-				throw new NotSupportedException();
-				//var l = Read(peekBuffer!, 0, 1);
-				//position -= l;
-				//return l == 0 ? -1 : peekBuffer![0];
-			}
-
-			public override int Read()
-			{
-				var l = Read(peekBuffer!, 0, 1);
-				return l == 0 ? -1 : peekBuffer![0];
-			}
+		public override int Read()
+		{
+			var l = Read(peekBuffer!, 0, 1);
+			return l == 0 ? -1 : peekBuffer![0];
 		}
 	}
 }
