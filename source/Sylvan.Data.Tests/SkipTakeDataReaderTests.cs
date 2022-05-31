e03693f4612ad333a1f218573240f21d01c0cf04
MarkPflug <MarkPflug@users.noreply.github.com>
2022-05-31 08:30:26 -0700

    Implement DbDataReader.Skip/Take

---
+++ source/Sylvan.Data.Tests/SkipTakeDataReaderTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Sylvan.Data;
+
+public class SkipTakeDataReaderTests
+{
+	[Fact]
+	public void TakeTest()
+	{
+		var seq = Enumerable.Range(0, 100).Select(i => new { Name = "test" + i, Value = i });
+		var reader = seq.AsDataReader();
+		reader = reader.Take(10);
+
+		for(int i = 0; i < 10; i++)
+		{
+			Assert.True(reader.Read());
+		}
+		Assert.False(reader.Read());
+	}
+
+	[Fact]
+	public void SkipTest()
+	{
+		var seq = Enumerable.Range(0, 100).Select(i => new { Name = "test" + i, Value = i });
+		var reader = seq.AsDataReader();
+		reader = reader.Skip(60);
+
+		for (int i = 0; i < 40; i++)
+		{
+			Assert.True(reader.Read());
+		}
+		Assert.False(reader.Read());
+	}
+
+	[Fact]
+	public void SkipTakeTest()
+	{
+		var seq = Enumerable.Range(0, 100).Select(i => new { Name = "test" + i, Value = i });
+		var reader = seq.AsDataReader();
+		reader = reader.Skip(60).Take(20);
+
+		for (int i = 0; i < 20; i++)
+		{
+			Assert.True(reader.Read());
+			Assert.Equal(60 + i, reader.GetInt32(1));
+		}
+		Assert.False(reader.Read());
+	}
+}
---
+++ source/Sylvan.Data/SkipTakeDataReader.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+
+namespace Sylvan.Data;
+
+sealed class SkipTakeDataReader : DataReaderAdapter
+{
+	long skip;
+	long take;
+
+	public SkipTakeDataReader(DbDataReader dr, long skip, long take) : base(dr)
+	{
+		this.skip = skip;
+		this.take = take;
+	}
+
+	public override bool Read()
+	{
+		while (skip > 0)
+		{
+			skip -= 1;
+			if (!Reader.Read())
+			{
+				return false;
+			}
+		}
+		switch(take)
+		{
+			case 0:
+				break;
+			default:
+				take -= 1;
+				goto case -1;
+			case -1:
+				return Reader.Read();
+		}
+		return false;
+	}
+}
