39a883f1bd8d651581bbc21174fb10bdec7c9528
MarkPflug <MarkPflug@users.noreply.github.com>
2023-03-24 14:07:23 -0700

    sylvan.data DynamicDataReader test

---
+++ source/Sylvan.Data.Tests/DynamicDataReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using Sylvan.Data.Csv;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Sylvan.Data;
+
+public class DynamicDataReaderTests
+{
+	[Fact]
+	public void Test1()
+	{
+		var data = "a,b,c\n1,2022-11-12,12.3\n";
+		var csv = CsvDataReader.Create(new StringReader(data));
+		var dr = new DynamicDataReader(csv);
+
+		Assert.True(dr.Read());
+
+		Assert.Equal(typeof(object), dr.GetFieldType(0));
+		Assert.Equal(typeof(object), dr.GetFieldType(1));
+		Assert.Equal(typeof(object), dr.GetFieldType(2));
+
+		Assert.Equal(1, dr.GetValue(0));
+		Assert.Equal(new DateTime(2022, 11, 12), dr.GetValue(1));
+		Assert.Equal(12.3m, dr.GetValue(2));
+	}
+}
