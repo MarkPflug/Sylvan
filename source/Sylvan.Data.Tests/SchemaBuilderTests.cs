8a9e20ce148dfdaab36c8d61cb0785fa18984ab8
MarkPflug <MarkPflug@users.noreply.github.com>
2025-01-16 14:32:44 -0800

    Test to verify that schema builder handles nullable types correctly.

---
+++ source/Sylvan.Data.Tests/SchemaBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Sylvan.Data;
+
+public class SchemaBuilderTests
+{
+	[Fact]
+	public void SchemaBuilderNullabilityTest()
+	{
+		var schema = 
+			new Schema.Builder()
+			.Add<int?>("Id")
+			.Add<double?>("Value")
+			.Add<long>("Count")
+			.Build();
+
+		Assert.True(schema[0].AllowDBNull);
+		Assert.Equal(typeof(int), schema[0].DataType);
+
+		Assert.True(schema[1].AllowDBNull);
+		Assert.Equal(typeof(double), schema[1].DataType);
+
+		Assert.False(schema[2].AllowDBNull);
+		Assert.Equal(typeof(long), schema[2].DataType);
+	}
+}
